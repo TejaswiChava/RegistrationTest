621a212551aed7d77f1801af3f938d703a4d5306
TejaswiChava <teja.chava4@gmail.com>
2020-06-17 18:35:39 +0530

    Update Registration.test.js

--- Registration.test.js
+++ Registration.test.js
@@ -1,43 +1,19 @@
 import React from 'react' ;
-import { mount ,shallow} from 'enzyme';
+import { mount } from 'enzyme';
 import Registration from './Registration';
-import TextField from '@material-ui/core/TextField';
 
 
 describe('<Registration />', () => {
-    // jest.mock('@material-ui/core/TextField', () => () => null)
-    const testValues = {
-        firstName: 'Leo',
-        lastName: 'Master',
-        phoneNumber: '9186869897',
-        emailAddress: 'leo.master@gmail.com',
-        userID: 'LeoMaster',
-        memberID: '123456',
-        address: '',
-        city: '',
-        state: '',
-        postalCode: '',
-        secQs1: 'Security Question 1',
-        secQs2: 'Security Question 2',
-        secQs3: 'Security Question 3',
-        answer1: 'Answer 1',
-        answer2: 'Answer 2',
-        answer3: 'Answer 3',
-        
-    };
     
-
     it('should open modal dialog', () => {
-       
-        const onSubmitSpy = jest.fn();
         const component = mount(
             <Registration/>
         );
+        expect(component.find('#submitButton').exists()).toEqual(true)
+        component.find('#submitButton').at(0).simulate('click');
+        component.update();
         expect(component.find('.custom-dialog').at(0).prop('open')).toBe(false)
-         expect(component.find('#submitButton').exists()).toEqual(true)
-         component.find('#submitButton').at(0).simulate('click');
-         component.update()
-         expect(component.find('.custom-dialog').at(0).prop('open')).toBe(true)
+
        
     });
 
@@ -46,14 +22,25 @@ describe('<Registration />', () => {
         const component = mount(
             <Registration/>,{fallbackIO}
         );
-        component.find('#standard-firstname').last().simulate('change', {target: {value: 'Leo'}})
-        const lastName = component.find('#standard-lastname').last()
-        lastName.getDOMNode().value = testValues.lastName
-        lastName.simulate('change')
-        expect(component.find('#standard-firstname').last().prop('value')).toBe('Leo')
+        component.find('#standard-firstname').last().simulate('change', {target: {value: 'Leo'}});
+        component.find('#standard-lastname').last().simulate('change', {target: {value: 'Martin'}});
+        component.find('#date-picker-dob').last().simulate('change', {target: {value: '05/01/2020'}});
+        component.find('#standard-phone').last().simulate('change', {target: {value: '9196969330'}});
+        component.find('#standard-email').last().simulate('change', {target: {value: 'leomartin@gmail.com'}});
+        component.find('#standard-username').last().simulate('change', {target: {value: 'LeoMartin'}});
+        component.find('#standard-memberid').last().simulate('change', {target: {value: '123456'}});
+        component.find('#standard-sq1').last().simulate('change', {target: {value: 'Security Question 1?'}});
+        component.find('#standard-sq2').last().simulate('change', {target: {value: 'Security Question 2?'}});
+        component.find('#standard-sq3').last().simulate('change', {target: {value: 'Security Question 3?'}});
+        component.find('#standard-ans1').last().simulate('change', {target: {value: 'Security Answer 1'}});
+        component.find('#standard-ans2').last().simulate('change', {target: {value: 'Security Answer 2'}});
+        component.find('#standard-ans3').last().simulate('change', {target: {value: 'Security Answer 3'}});
+        expect(component.find('#standard-firstname').last().prop('value')).toBe('Leo');
+        expect(component.find('#standard-lastname').last().prop('value')).toBe('Martin');
+        expect(component.find('#date-picker-dob').last().prop('value')).toBe('05/01/2020');
         component.find('#submitButton').last().simulate('click');
-         console.log(fallbackIO.mock.calls[0])
-        // expect(fallbackIO).toBeCalledTimes(3)
+        component.update();
+        expect(component.find('.custom-dialog').at(0).prop('open')).toBe(false)
 
     });
 })
